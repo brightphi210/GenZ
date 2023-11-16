970ad47129653b37830ec87e41e28a115edc4c6e
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-11-16 11:50:39 -0800

    latest

--- GenZ/settings.py
+++ GenZ/settings.py
@@ -188,7 +188,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 import os
 # Directory where uploaded media is saved.
@@ -197,10 +197,8 @@ MEDIA_URL = '/media/'  # Public URL at the browser
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "media")]
+STATIC_URL = "media/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", 'media')
-
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
